5f6af0e7566a472fae48a109982b784b972c7c18
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-02-29 17:59:21 +0000

    Just started

---
+++ src/js/component/playlist.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {useState, useEffect } from "react";
+
+
+//include images into your bundle
+
+//create your first component
+export const Playlist = () => {
+
+useEffect(()=>{
+
+
+},[]);
+
+// function func1 (){
+
+// }
+
+// function func1 (){
+    
+// }
+// function func1 (){
+    
+// }
+// function func1 (){
+    
+// }
+// function func1 (){
+    
+// }
+
+return (
+		<div className="">
+		 <audio controlsList="next" title="Track 1" >
+    			<source src="https://archive.org/download/calexico2006-12-02..flac16/calexico2006-12-02d1t02.mp3" data-track-number="1" />
+    			<source src="https://archive.org/download/ra2007-07-21/ra2007-07-21d1t05_64kb.mp3" data-track-number="2" />
+                <source src="https://archive.org/download/calexico2006-12-02..flac16/calexico2006-12-02d1t02.mp3" data-track-number="1" />
+    			<source src="https://archive.org/download/ra2007-07-21/ra2007-07-21d1t05_64kb.mp3" data-track-number="2" />
+                <source src="https://archive.org/download/calexico2006-12-02..flac16/calexico2006-12-02d1t02.mp3" data-track-number="1" />
+    			<source src="https://archive.org/download/ra2007-07-21/ra2007-07-21d1t05_64kb.mp3" data-track-number="2" />                         
+                Your browser does not support HTML5 audio.
+                </audio>
+			
+		</div>
+	);
+};
+
+
