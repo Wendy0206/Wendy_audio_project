ecbd0310ff08169d415b0d037792b9ccb51ea85a
Wendy Donley Pierre <155206385+Wendy0206@users.noreply.github.com>
2024-07-03 01:47:45 +0000

    Adding spotify api

--- src/js/component/playlist.jsx
+++ src/js/component/playlist.jsx
@@ -18,22 +18,18 @@ export const Playlist = () => {
 	let startPress = null;
 	let playlistSong = [
 		{
+			title: "Thunder Cats",
+			author: "Moonra",
+			url:
+				"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
+		},{
 			title: "South Park",
-			id: "south-park",
 			author: "Kyle",
 			url:
 				" https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
 		},
 		{
-			title: "Thunder Cats",
-			id: "thundercats",
-			author: "Moonra",
-			url:
-				"https://assets.breatheco.de/apis/sound/files/cartoons/songs/thundercats.mp3"
-		},
-		{
 			title: "X-Men",
-			id: "x-men",
 			author: "Profesor",
 			url:
 				"https://assets.breatheco.de/apis/sound/files/cartoons/songs/x-men.mp3"
@@ -42,64 +38,67 @@ export const Playlist = () => {
 
 	var audioTest = new Audio;
 
-
 	const getSongs = () => {
 
-
-		const url = 'https://spotify23.p.rapidapi.com/tracks/?ids=4WNcduiCmDNfmTEz7JvmLv';
-		const options = {
-			method: 'GET',
-			headers: {
-				'x-rapidapi-key': 'b7d8c24116msh18d47855c91a4c6p129b19jsn25ca23c193e6',
-				'x-rapidapi-host': 'spotify23.p.rapidapi.com'
-			}
+		var client_id = 'cfc0b81bea4641cbb51c4e9a3d2ecaa8';
+		var client_secret = '7c4058cf53d745a39017ec617e368ecf';
+		
+		var authOptions = {
+		  url: 'https://accounts.spotify.com/api/token',
+		  headers: {
+			'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+		  },
+		  form: {
+			grant_type: 'client_credentials'
+		  },
+		  json: true
 		};
-
-		fetch(url, options)
-			.then(res => {
-				if (!res.ok) { throw error(res.statusText) }
-
-				return res.json;
-
-			})
-			.then(response => {
-				console.log(response);
-			})
-			.then(error => console.log(error))
+		
+		request.post(authOptions, function(error, response, body) {
+		  if (!error && response.statusCode === 200) {
+			var token = body.access_token;
+			console.log('this is the token we get fro mthe token : '+ token);
+		  }
+		});
 
 	}
 
-
+	useEffect(() => {
+		
+//getSongs();
+	}, []);	
 
 	useEffect(() => {
-		if (playT.current == true) {
-		
-			// interval = setInterval(() => {	
-			// 		setSongP(songP + 1);
-			// 		 console.log('this is where we are in that song : ' + setStepS);
-			// }, stepS);
-			// return () => clearInterval(interval);
+		if (playT.current == true && songP<100) {
+
+			interval = setInterval(() => {	
+					setSongP(songP + 1);
+					
+			}, stepS);
+			return () => clearInterval(interval);
 		}
 
-		else {
+		else if( songP==100) {
 			setSongP(0);
+			let next= currentSong.current+1;
+			next= currentSong.current==playlistSong.length-1? 0 : next;
+			audioTest.src = playlistSong[next].url;
+		audioTest.play();
 			clearInterval(interval);
 		}
 
-	}, [stepS,songP]);
+	}, [stepS, songP]);
+
 
 
 
 	function play_function(id) {
-
-
 		// check if user click on the play button
 		if (id == -1) {
 			if (playT.current == false) {
 				setPlayStatus('fa-solid fa-pause fa-2xl');
 				audioTest.src = playlistSong[currentSong.current].url;
 				audioTest.play();
-			//	setStepS(Math.floor(audioTest.duration / 100));
 				playT.current = true;
 			}
 
@@ -113,22 +112,18 @@ export const Playlist = () => {
 
 		// else user click on a song directly
 		else {
-			
+
 			if (playT.current == false) {
-					playT.current = true;
-			console.log('this is the song we got : ' + id+ ' and this is playT right now : ', playT.current);
+				playT.current = true;
 				setPlayStatus('fa-solid fa-pause fa-2xl');
 				audioTest.src = playlistSong[id].url;
 				audioTest.play();
-			//	setStepS(Math.floor(audioTest.duration / 100));
-			//	console.log('this is where we are in that song : ' +audioTest.duration);
-			
 				currentSong.current = id;
 			}
 
 			else {
 				setPlayStatus('fa-solid fa-play fa-2xl');
-			//	audioTest.currentTime = 0;
+				//	audioTest.currentTime = 0;
 				audioTest.play();
 				playT.current = false;
 			}
@@ -138,9 +133,8 @@ export const Playlist = () => {
 	}
 
 	function switch_function(pos) {
-		if(currentSong.current=== playlistSong.length-1 && pos==1){pos=-currentSong.current}
-		currentSong.current =  currentSong.current + pos;
-		console.log('this si the the song we are playing now', currentSong.current);
+		if (currentSong.current === playlistSong.length - 1 && pos == 1) { pos = -currentSong.current }
+		currentSong.current = currentSong.current + pos;
 		audioTest.src = playlistSong[currentSong.current].url;
 		audioTest.play();
 	}
@@ -165,17 +159,17 @@ export const Playlist = () => {
 			audioTest.muted = true;
 		}
 		else {
-if(volumeT>0){
-	setVolumeT(volumeT - 0.1);
-	audioTest.volume = volumeT;
-}
-		
+			if (volumeT > 0) {
+				setVolumeT(volumeT - 0.1);
+				audioTest.volume = volumeT;
+			}
+
 		}
 
 
 	}
-	
-	
+
+
 	function random_function() {
 
 	}
@@ -187,38 +181,38 @@ if(volumeT>0){
 			audioTest.volume = volumeT;
 		}
 		else {
-			if(volumeT<1.0){
-	setVolumeT(volumeT + 0.1);
-			audioTest.volume = volumeT;
-			setVolumeE('fa-solid fa-volume-low fa-xl');
+			if (volumeT < 1.0) {
+				setVolumeT(volumeT + 0.1);
+				audioTest.volume = volumeT;
+				setVolumeE('fa-solid fa-volume-low fa-xl');
 			}
-		
+
 		}
 
 	}
 
-// 	if (volumeT == 0) {
-// 		setVolumeT(0.5);
-// 	}
-// 	else {
-// 		if(volumeT<1){
-// 			setVolumeT(volumeT + 0.1);
-// 			setVolumeE('fa-solid fa-volume-low fa-xl');
-// 		}
-// 	}
-// }else{
+	// 	if (volumeT == 0) {
+	// 		setVolumeT(0.5);
+	// 	}
+	// 	else {
+	// 		if(volumeT<1){
+	// 			setVolumeT(volumeT + 0.1);
+	// 			setVolumeE('fa-solid fa-volume-low fa-xl');
+	// 		}
+	// 	}
+	// }else{
 
-// 	if(volumeT<1 && volumeT>0){
-// 		setVolumeT(volumeT - 0.1);
-// 		setVolumeE('fa-solid fa-volume-low fa-xl');
-// 	}
-// }
-// audioTest.volume = volumeT;
-
-
-
-
-
+	// 	if(volumeT<1 && volumeT>0){
+	// 		setVolumeT(volumeT - 0.1);
+	// 		setVolumeE('fa-solid fa-volume-low fa-xl');
+	// 	}
+	// }
+	// audioTest.volume = volumeT;
+const get_duration=(e)=>{
+	setStepS(Math.floor((e.target.duration * 1000) / 100));
+	let test= Math.floor((e.target.duration * 1000) / 100);
+	console.log(' This is the step at which we are going to move : '+test);
+}
 
 	return (
 		<div className="container">
@@ -237,7 +231,7 @@ if(volumeT>0){
 						</div>
 					)
 				}
-				<audio ref={(e) => audioTest = e} preload="metadata" id="testTone" />
+				<audio ref={(e) => audioTest = e} preload="metadata" id="testTone" onLoadedMetadata={(e)=>get_duration(e)} />
 
 			</div>
 
